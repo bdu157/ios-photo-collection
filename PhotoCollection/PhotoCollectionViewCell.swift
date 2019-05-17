eb146eabdaf33aa6d2b691e8b1cd9ad8d0b80ec9
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-16 20:25:18 -0700

    set up storyboard

---
+++ PhotoCollection/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Dongwoo Pae on 5/16/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
---
+++ PhotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Dongwoo Pae on 5/16/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    var themeHelper: ThemeHelper?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    @IBAction func SelectOrangeTheme(_ sender: Any) {
+    }
+    
+}
